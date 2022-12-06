8a7c6ccafa024c03a65be71f289b97022095d74f
Muniyaraja98 <smuniyaraja98@gmail.com>
2022-12-06 11:00:01 +0530

    Multiple image upload

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  selectedImages: any = [];
+  previewImages: any = [];
+
+  @ViewChild('openImg') input: ElementRef<HTMLElement> | any;
 
   constructor() {}
 
+  imageFiles(event: any) {
+    // console.log(event);
+    this.selectedImages.push(...event.target.files);
+    for (let i = 0; i < this.selectedImages.length; i++) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.previewImages.push(e.target.result);
+      };
+      console.log(this.selectedImages[i]);
+      reader.readAsDataURL(this.selectedImages[i]);
+    }
+  }
+
+  imageReorder(ev: any) {
+    // console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+    this.previewImages = ev.detail.complete(this.previewImages);
+    var newArr = this.selectedImages;
+    [newArr[ev.detail.from], newArr[ev.detail.to]] = [
+      newArr[ev.detail.to],
+      newArr[ev.detail.from],
+    ];
+    console.log(newArr);
+  }
+
+  undoImage(event: any) {
+    this.previewImages.splice(event, 1);
+    this.selectedImages.splice(event, 1);
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  selectedFiles?: any = [];
+  progressInfos: any[] = [];
+  message: string[] = [];
+  previews: string[] = [];
+  product_id: any;
+
+  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup | any;
+  @ViewChild('image') input: ElementRef | any;
 
   constructor() {}
 
+  selectFiles(event: any): void {
+    this.message = [];
+    this.progressInfos = [];
+    this.selectedFiles.push(...event.target.files);
+    this.previews = [];
+    const numberOfFiles = this.selectedFiles.length;
+
+    for (let i = 0; i < numberOfFiles; i++) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.previews.push(e.target.result);
+      };
+      console.log(this.selectedFiles[i]);
+      reader.readAsDataURL(this.selectedFiles[i]);
+    }
+  }
+
+  doReorder(ev: any) {
+    this.previews = ev.detail.complete(this.previews);
+    var newArr = this.selectedFiles;
+    [newArr[ev.detail.from], newArr[ev.detail.to]] = [
+      newArr[ev.detail.to],
+      newArr[ev.detail.from],
+    ];
+  }
+
+  toggleReorderGroup() {
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
+  }
+
+  removeImage(a: any) {
+    this.previews.splice(a, 1);
+    this.selectedFiles.splice(a, 1);
+  }
 }
